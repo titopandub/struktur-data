aa29798ea62c2b8c3af06ab5fb9f987b8a6fae03
Tito Pandu Brahmanto <titopandub@users.noreply.github.com>
2021-11-01 07:26:13 +0700

    Diskusi 3

---
+++ src/com/company/ArrayDuaDimensi.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class ArrayDuaDimensi {
+    public static void main(String[] args) {
+        String[][] arrayDuaDimensi = { {"a", "b", "c"}, {"d", "e", "f"} };
+        System.out.println(Arrays.deepToString(arrayDuaDimensi));
+    }
+}
